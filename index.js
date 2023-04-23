d71c557cc14a4059c6f409f75cf11001c3456df6
Tautvilas Mečinskas <connect@tautvilas.lt>
2023-04-23 17:33:18 +0300

    simple instance setup using libp2p

---
+++ index.js
@@ -0,0 +1,33 @@
+import { createLibp2p } from 'libp2p'
+import { tcp } from '@libp2p/tcp'
+import { noise } from '@chainsafe/libp2p-noise'
+import { mplex } from '@libp2p/mplex'
+
+const main = async () => {
+  const node = await createLibp2p({
+    addresses: {
+      // add a listen address (localhost) to accept TCP connections on a random port
+      listen: ['/ip4/127.0.0.1/tcp/0']
+    },
+    transports: [tcp()],
+    connectionEncryption: [noise()],
+    streamMuxers: [mplex()]
+  })
+
+  // start libp2p
+  await node.start()
+  console.log('libp2p has started')
+
+  // print out listening addresses
+  console.log('listening on addresses:')
+  node.getMultiaddrs().forEach((addr) => {
+    console.log(addr.toString())
+  })
+
+  // stop libp2p
+  await node.stop()
+  console.log('libp2p has stopped')
+}
+
+main().then().catch(console.error)
+
