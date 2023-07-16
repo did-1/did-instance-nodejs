44480b666de8250402b20b3313fd4e2ac40d1399
Tautvilas Mečinskas <connect@tautvilas.lt>
2023-07-16 18:02:58 +0300

    add enpoint for public/private key generation

--- src/http.js
+++ src/http.js
@@ -1,9 +1,19 @@
+import crypto from 'crypto'
 import express from 'express'
 
 const app = express()
 
 app.get('/', (req, res) => {
-  return res.send('Hello world!')
+  return res.send('This is DID instance node')
+})
+
+app.post('/keys', (req, res) => {
+  const pair = crypto.generateKeyPairSync('ec', {
+    namedCurve: 'secp256k1'
+  })
+  const privateKey = pair.publicKey.export({ type: 'spki', format: 'pem' })
+  const publicKey = pair.privateKey.export({ type: 'pkcs8', format: 'pem' })
+  return res.send({ privateKey, publicKey })
 })
 
 export default app
