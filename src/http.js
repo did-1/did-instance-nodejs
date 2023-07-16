884b4835684a2f2e344e468648042275e5f42a62
Tautvilas Mečinskas <connect@tautvilas.lt>
2023-07-16 20:15:06 +0300

    add cors, fix keys logic

--- src/http.js
+++ src/http.js
@@ -1,7 +1,9 @@
 import crypto from 'crypto'
 import express from 'express'
+import cors from 'cors'
 
 const app = express()
+app.use(cors())
 
 app.get('/', (req, res) => {
   return res.send('This is DID instance node')
@@ -11,8 +13,8 @@ app.post('/keys', (req, res) => {
   const pair = crypto.generateKeyPairSync('ec', {
     namedCurve: 'secp256k1'
   })
-  const privateKey = pair.publicKey.export({ type: 'spki', format: 'pem' })
-  const publicKey = pair.privateKey.export({ type: 'pkcs8', format: 'pem' })
+  const privateKey = pair.privateKey.export({ type: 'pkcs8', format: 'pem' })
+  const publicKey = pair.publicKey.export({ type: 'spki', format: 'pem' })
   return res.send({ privateKey, publicKey })
 })
 
