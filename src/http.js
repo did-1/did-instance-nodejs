a2b27da42562a5b541c1a89f9ed382cb8d0fcd72
Tautvilas Mečinskas <connect@tautvilas.lt>
2023-07-17 16:15:09 +0300

    feat: check if pubkey exists on domain

--- src/http.js
+++ src/http.js
@@ -1,9 +1,11 @@
 import crypto from 'crypto'
 import express from 'express'
 import cors from 'cors'
+import fetch from 'node-fetch'
 
 const app = express()
 app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) => {
   return res.send('This is DID instance node')
@@ -16,6 +18,21 @@ app.post('/keys', (req, res) => {
   const privateKey = pair.privateKey.export({ type: 'pkcs8', format: 'pem' })
   const publicKey = pair.publicKey.export({ type: 'spki', format: 'pem' })
   return res.send({ privateKey, publicKey })
+})
+
+app.post('/users/:domain/validate', async (req, res) => {
+  const domain = req.params.domain
+  const publicKey = req.body.publicKey
+  let valid = false
+  try {
+    const resp = await fetch(['http:/', domain, 'did.pem'].join('/'))
+    const text = await resp.text()
+    console.log(text)
+    if (publicKey && text.replace(/\n/g, '') === publicKey.replace(/\n/g, '')) {
+      valid = true
+    }
+  } catch {}
+  return res.send({ domain, valid })
 })
 
 export default app
