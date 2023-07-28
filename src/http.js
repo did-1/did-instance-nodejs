9afe2d7a7edc3c8e6078f80a780fd58e48cf3b4b
Tautvilas Mečinskas <connect@tautvilas.lt>
2023-07-28 15:00:34 +0300

    refactor domain validation

--- src/http.js
+++ src/http.js
@@ -78,9 +78,9 @@ app.post('/users/:domain/path/validate', async (req, res) => {
   return res.send({ domain, valid })
 })
 
-app.post('/users/:domain/post', async (req, res) => {
-  const domain = req.params.domain.toLowerCase()
-  const body = req.body
+// sanitize and validate domain name
+function validateDomainName(domain = '') {
+  domain = domain.toLowerCase()
   const domainChecks = [
     {
       validator: (d = '') => {
@@ -101,11 +101,30 @@ app.post('/users/:domain/post', async (req, res) => {
   for (let i = 0; i < domainChecks.length; i++) {
     const check = domainChecks[i]
     if (!check.validator(domain)) {
-      return res.send({ error: check.message })
+      return { valid: false, message: check.message }
     }
   }
+  return { valid: true, value: domain }
+}
+
+app.post('/users/:domain/post', async (req, res) => {
+  const body = req.body
+  const domainValidation = validateDomainName(req.params.domain)
+  if (!domainValidation.valid) {
+    return res.send({ error: 'User domain invalid' + domainValidation.message })
+  }
+  const ownerDomain = domainValidation.value
+
+  const postDomainValidation = validateDomainName(req.body.domain)
+  if (!domainValidation.valid) {
+    return res.send({
+      error: 'Post domain invalid:' + domainValidation.message
+    })
+  }
+  const postDomain = postDomainValidation.value
+
   // 2. get public key
-  const publicKeyUrl = ['http:/', domain, 'did.pem'].join('/')
+  const publicKeyUrl = ['http:/', ownerDomain, 'did.pem'].join('/')
   const resp = await fetch(publicKeyUrl)
   const { hash, blockHash, signature, path } = req.body
   const publicKeyPem = await resp.text()
@@ -123,16 +142,16 @@ app.post('/users/:domain/post', async (req, res) => {
     return res.send({ error: `Invalid public key` })
   }
   // 2. download post from url
-  const postUrl = ['http:/', req.body.domain, path].join('/')
+  const postUrl = ['http:/', postDomain, path].join('/')
   const postResp = await fetch(postUrl)
   const post = await postResp.text()
-  // TODO: target domain name and path validation
+  // TODO: target domain name and path validation, remove repeating slashes, sart end slashes
   // TODO: do proper validation with HTML parser
   if (!post.includes('did:content')) {
     return res.send({ error: `Valid DID post not found at ${postUrl}` })
   }
   // 3. validate signature
-  const message = [blockHash, req.body.domain, path, hash].join('/')
+  const message = [blockHash, postDomain, path, hash].join('/')
   const verify = crypto.createVerify('sha256')
   verify.update(message)
   verify.end()
@@ -158,14 +177,21 @@ app.post('/users/:domain/post', async (req, res) => {
     `INSERT INTO posts (signature, domain, hash, path, block) VALUES (?, ?, ?, ?, ?)`
   )
   const sigHex = signature.map((num) => num.toString(16)).join('')
-  stmt.run(sigHex, req.body.domain, hash, path, blockHash, (err) => {
-    if (err) {
-      console.error(err)
-      return res.send({ domain, body, error: 'DB insert failed' })
-    } else {
-      return res.send({ domain, body, error: 'DONE' })
+  stmt.run(
+    sigHex,
+    ownerDomain,
+    hash,
+    [postDomain, path].join('/'),
+    blockHash,
+    (err) => {
+      if (err) {
+        console.error(err)
+        return res.send({ error: 'DB insert failed' })
+      } else {
+        return res.send({ error: 'DONE' })
+      }
     }
-  })
+  )
   // 8. publish message on the network
   // return res.send({ domain, body, error: 'Not implemented' })
 })
